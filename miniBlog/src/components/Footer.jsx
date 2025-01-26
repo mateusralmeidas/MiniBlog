6baf13e2df58c5b1d9b7f9e75004066db7f214f5
Mateus Solar <mateusr.almeidas@gmail.com>
2025-01-26 20:16:04 +0800

    css navbar and footer

--- miniBlog/src/components/Footer.jsx
+++ miniBlog/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
         <h3>Tell me what you have interest in</h3>
         <p>Mini Blog &copy; 2025</p>
     </footer>
